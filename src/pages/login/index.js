73cdb8974540ecafaf955d593ea9f75092f6d316
giselefgois <g.ferreira.de.gois@accenture.com>
2023-02-25 22:49:06 -0300

    tela de login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import banner from '../../assets/kanashiro.png'
 
 import './style.css';
 
 export default function Login() {
 
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+<body>
+    <main>
+        <h1>Login</h1>
+
+        <div class="alternative">
+        </div>
+
+        <form action="">
+
+            <label for="text">
+                <span>Usuario</span>
+                <input type="text" id="text" name="text"/>
+            </label>
+            <label for="password">
+                <span>Senha</span>
+                <input type="password" id="password" name="password"/>
+            </label>
+
+            <input type="submit" value="Sign Up"/>
+        </form>
+    </main>
+
+    <section class="images">
+        <img src={banner} alt="logo"/>
+        <div class="circle"></div>
+    </section>
+</body>
   );
 }
