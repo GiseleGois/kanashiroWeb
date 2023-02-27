4b8239bc78706ecda4dd35a671a49193c1faa759
giselefgois <g.ferreira.de.gois@accenture.com>
2023-02-27 11:59:15 -0300

    exibir apenas a pagina de pedidos

--- src/routes.js
+++ src/routes.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Home from './pages/home';
-import Login from './pages/login';
-import Register from './pages/register';
+// import Login from './pages/login';
+// import Register from './pages/register';
 
 function Routes() {
   return (
     <Router>
-      <Route path='/' exact component={Login} />
-      <Route path='/register' component={Register} />
-      <Route path='/home' component={Home} />
+      <Route path='/' exact component={Home} />
+      {/* <Route path='/register' component={Register} />
+      <Route path='/home' component={Home} /> */}
     </Router>
   );
 }
