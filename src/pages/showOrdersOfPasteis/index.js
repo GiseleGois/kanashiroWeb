b4fe22a87a070428bf8e03a02d2591159024e017
giselefgois <g.ferreira.de.gois@accenture.com>
2023-10-30 04:57:02 -0300

    ajuste de layout na tabela de pasteis

--- src/pages/showOrdersOfPasteis/index.js
+++ src/pages/showOrdersOfPasteis/index.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { ArrowLeft } from 'react-feather';
 import './style.css';
 import { listProducts, listOrdersToExhibitInOrderPage } from '../../service';
 
 function ShowOrdersOfPasteis() {
+  const history = useHistory();
   const [products, setProducts] = useState([]);
   const [orders, setOrders] = useState([]);
   const [resumeTotal, setResumeTotal] = useState([]);
@@ -14,8 +17,9 @@ function ShowOrdersOfPasteis() {
     const fetchProducts = async () => {
       try {
         const productsData = await listProducts();
-        productsData.sort((a, b) => a.priority - b.priority);
-        setProducts(productsData);
+        const filteredProducts = productsData.filter(product => product.type !== 'salgado');
+        filteredProducts.sort((a, b) => a.priority - b.priority);
+        setProducts(filteredProducts);
         setLoadingProducts(false);
       } catch (error) {
         console.error('Error fetching products:', error);
@@ -37,11 +41,16 @@ function ShowOrdersOfPasteis() {
 
       try {
         const ordersData = await listOrdersToExhibitInOrderPage(startDate, endDate);
-        const extractedOrders = ordersData.combineItems;
-        const extractedResume = ordersData.groupedProducts;
+
+        const filteredOrders = ordersData.combineItems.filter((order) => {
+          return order.items.some((item) => item.type !== 'salgado');
+        });
+
+        const extractedResume = ordersData.groupedProducts.filter(item => item.type !== 'salgado');
+
         const extractedTotalPasteis = ordersData.totalQuantities.totalNonSalgados;
 
-        setOrders(extractedOrders);
+        setOrders(filteredOrders);
         setResumeTotal(extractedResume);
         setTotalQuantitiesOfPasteis(extractedTotalPasteis);
 
@@ -58,6 +67,7 @@ function ShowOrdersOfPasteis() {
     fetchOrders();
   }, []);
 
+
   const getItemQuantity = (productId, userFullName) => {
     const order = orders.find(order => order.userFullName === userFullName);
     if (order) {
@@ -67,68 +77,68 @@ function ShowOrdersOfPasteis() {
     return 0;
   };
 
+  // const backPage = () => {
+  //   history.push('/orders');
+  // };
+
   const userFullNames = [...new Set(orders.map(order => order.userFullName))];
   userFullNames.sort((a, b) => a.localeCompare(b));
 
-  return (
-    <div className="print-container">
-      {loadingProducts ? (
-        <div className="loading">Carregando produtos...</div>
-      ) : (
-        <table className="print-table">
-          <thead>
-            <tr>
-              <th>Descrição</th>
-              {userFullNames.map(userFullName => (
-                <th key={userFullName}>{userFullName}</th>
-              ))}
-              <th>Total</th>
-            </tr>
-          </thead>
-          <tbody>
-            {products
-              .filter(product => product.type !== 'salgado')
-              .map((product) => {
-                const productQuantities = userFullNames.map(userFullName =>
-                  getItemQuantity(product.id, userFullName)
-                );
-                const totalQuantity = totalQuantitiesOfProducts[product.id] || 0;
+const shouldDisplayOrder = (order) => {
+  return order.items.some((item) => products.find((product) => product.id === item.productId && product.type !== 'salgado'));
+};
 
-                const shouldHideRow = totalQuantity === 0 && productQuantities.every(qty => qty === '');
+return (
+  <div className="print-container">
+    {loadingProducts ? (
+      <div className="loading">Carregando produtos...</div>
+    ) : (
+      <table className="print-table">
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            {userFullNames.map((userFullName) => (
+              <th key={userFullName}>{userFullName}</th>
+            ))}
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products
+            .filter((product) => {
+              return orders.some((order) => shouldDisplayOrder(order) && getItemQuantity(product.id, order.userFullName) > 0);
+            })
+            .map((product) => {
+              const productQuantities = userFullNames.map((userFullName) =>
+                getItemQuantity(product.id, userFullName)
+              );
+              const totalQuantity = totalQuantitiesOfProducts[product.id] || 0;
 
-                const rowClassName = `product-row ${product.type === 'M' ? 'blue-row' : ''} ${product.type === 'D' ? 'yellow-row' : ''}`;
+              const rowClassName = `product-row ${product.type === 'M' ? 'blue-row' : ''} ${product.type === 'D' ? 'yellow-row' : ''}`;
 
-                if (shouldHideRow) {
-                  return null;
-                }
-
-                return (
-                  <tr key={product.id} className={rowClassName}>
-                    <td className="product-name">{product.name}</td> {/* Apply product-name class here */}
-                    {userFullNames.map((userFullName, index) => (
-                      <td key={userFullName}>
-                        {productQuantities[index]}
-                      </td>
-                    ))}
-                    <td className='total-column'>
-                      {totalQuantity}
-                    </td>
-                  </tr>
-                );
-              })}
-            <tr>
+              return (
+                <tr key={product.id} className={rowClassName}>
+                  <td className="product-name">{product.name}</td>
+                  {userFullNames.map((userFullName, index) => (
+                    <td key={userFullName}>{productQuantities[index]}</td>
+                  ))}
+                  <td className="total-column">{totalQuantity}</td>
+                </tr>
+              );
+            })}
+          <tr>
             <td className="product-name">
               <strong>Total de pasteis</strong>
-              </td>
-              <td>
-                <strong>{totalQuantitiesOfPasteis}</strong>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      )}
-    </div>
-  );
+            </td>
+            <td>
+              <strong>{totalQuantitiesOfPasteis}</strong>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    )}
+  </div>
+);
 }
 
 export default ShowOrdersOfPasteis;
